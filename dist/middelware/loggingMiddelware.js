1d160527c65f45936b0fa60b40e90be3a2d5c7d6
junaid786-prog <junaidshoaib555@gmail.com>
2024-03-19 11:49:51 +0500

    log removed

--- dist/middelware/loggingMiddelware.js
+++ dist/middelware/loggingMiddelware.js
@@ -4,7 +4,6 @@
  */
 const loggingMiddleware = (config) => {
     // configure the middleware
-    console.log('Config:', config);
     return (req, res, next) => {
         console.log('Request::', req?.url);
         next();
